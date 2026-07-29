a8f48c9e2f9e6eac1384db7b29f117a35a434e92
t38748845-maker <t38748845@gmail.com>
2026-07-29 20:37:29 +0530

    Update Eular's_Number.py

--- Eular's_Number.py
+++ Eular's_Number.py
@@ -1,1 +1,23 @@
+# calculating compound interest in python
+
+from math import exp
+
+p = 100
+r = .20
+t = 2.0
+n = 12
+
+a = p * (1 + (r / n) ** (n * t))
+print(a)
+
+# calculating continuous interest in python...
+
+from math import exp
+
+p = 100
+r = 0.20
+t = 2.0
+
+a = p * exp(r * t)
+print(a)
 #

49a8e4eb8160b03848b0556807d0442d5472fe9e
t38748845-maker <t38748845@gmail.com>
2026-07-29 20:40:38 +0530

    Update Limit's.py

--- Limit's.py
+++ Limit's.py
@@ -1,1 +1,21 @@
-#
+# using sympy to calculate limits...
+from sympy import *
+
+x = symbols('x')
+f = 1 / x
+result = limit(f, x, oo)
+print(result)
+
+# displaying eular's number using sympy... 
+from sympy import *
+n = symbols('x')
+f = (1 + (1 / n)) ** n
+result = limit(f, n, oo)
+print(result) # E
+print(result.evalf())
+# output; 2.71828182845905
+
+# simple way to show eular's number...
+import math
+print(math.e)
+# output; 2.718281828459045

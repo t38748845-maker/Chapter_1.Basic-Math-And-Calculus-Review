3ef1b163edd068c992f1223e38615d7b86b6ca53
t38748845-maker <t38748845@gmail.com>
2026-07-29 20:44:13 +0530

    Update Summations.py

--- Summations.py
+++ Summations.py
@@ -1,1 +1,20 @@
-#
+# calculating partial derivatives with sympy...
+from sympy import *
+from sympy.plotting import plot3d
+
+# declare x and y to sympy...
+x, y = symbols('x y')
+
+# declearing the function...
+f = 2*x**3 + 3*y**3
+ 
+# calculate partial derivatives for x and y..
+dx_f = diff(f, x) 
+dy_f = diff(f, y)
+
+# printing...
+print(dx_f)
+print(dy_f)
+
+# plot the function...
+plot3d(f)

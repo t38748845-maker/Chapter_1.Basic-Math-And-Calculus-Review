7623c754ece663e68cf2efc5b99cf7249bc474e9
t38748845-maker <t38748845@gmail.com>
2026-07-29 19:15:02 +0530

    Create Order_Of_Operation.py

---
+++ Order_Of_Operation.py
@@ -0,0 +1,7 @@
+# Solving and expression in python... 
+my_value = 2 * (3 + 2) ** 2 / 5 - 4
+print(my_value)
+
+# Mahing use of parenthesis for clarity in python...
+my_value = 2 * ((3 + 2) ** 2 / 5) - 4
+print(my_value)
